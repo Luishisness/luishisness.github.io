5a61e953835bec6b0000ab16de54560f45ca44d9
Shawn Vega <shawn.p.vega@gmail.com>
2016-02-14 00:34:34 -0500

    content update

--- woodshop.js
+++ woodshop.js
@@ -31,12 +31,12 @@ $('#myCarousel').carousel({
 //$("#foo").append("<div>hello world</div>")
 //carouselList.appendTo('.carousel-inner');
 //$('<div class="item"><img src="/luishisness.github.io/images/chair.jpg" alt="chair"></div>').appendTo('.carousel-inner');
-$('<div class="item">' + carouselList +'<div>').appendTo('.carousel-inner');
+//$('<div class="item">' + carouselList +'<div>').appendTo('.carousel-inner');
 
-/*for(i=0; i<carouselList.length; i++){
+/**/for(i=0; i<carouselList.length; i++){
 
-	$('<div class="item">' + carouselList +'<div>').appendTo('.carousel-inner');
-}*/
+	$('<div class="item">' + carouselList[i] +'<div>').appendTo('.carousel-inner');
+}
 
 
 
