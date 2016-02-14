f4159666368012c9bd1490d06c20694b0979db36
Shawn Vega <shawn.p.vega@gmail.com>
2016-02-14 11:37:18 -0500

    content update

--- woodshop.js
+++ woodshop.js
@@ -35,7 +35,7 @@ $('#myCarousel').carousel({
 //$('<div class="item"><img src="/luishisness.github.io/images/chair.jpg" alt="chair"></div>').appendTo('.carousel-inner');
 //$('<div class="item">' + carouselList +'<div>').appendTo('.carousel-inner');
 
-//$('#myCarousel').hide();
+$('#myCarousel').hide();
 
 /*for(i=0; i<carouselList.length; i++){
 
@@ -46,9 +46,9 @@ $('#myCarousel').carousel({
 	listtoAppend.appendTo('.carousel-inner');
 }*/
 
-/*$(".grid-woodshop").click(function(){
+/**/$(".grid-woodshop").click(function(){
     $('#myCarousel').fadeToggle();
-});*/
+});
 
 
 
