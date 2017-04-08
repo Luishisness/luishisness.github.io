56f30fea06dbd4330e2971ea37d4ddff9d38701a
Shawn Vega <shawn.p.vega@gmail.com>
2017-04-08 16:54:32 -0400

    editing

--- woodshop_landing.js
+++ woodshop_landing.js
@@ -4,10 +4,10 @@ console.log("WoodShop Landing Page");
 
 
 
-jQuery( document ).ready(function() {
+/*jQuery( document ).ready(function() {
 
     jQuery(".mp-container-module").hover(function(e){
     	jQuery(this).animate({backgroundColor: "#ffffff" });
 	});
 
-});
+});*/
